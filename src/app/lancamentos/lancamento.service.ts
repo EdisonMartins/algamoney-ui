0ee51f6c2ed9ab50d632bb186ca31c8d43bafebb
EdisonMartins <martinsegmj@gmail.com>
2023-09-11 01:46:32 -0300

    18.6 - Atualização e busca por código

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,3 +1,4 @@
+import { Lancamento } from './../core/model';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
@@ -47,17 +48,17 @@ export class LancamentoService {
 
 
     return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
-    .toPromise()
-    .then((response: any) => {
-      const lancamentos = response['content'];
+      .toPromise()
+      .then((response: any) => {
+        const lancamentos = response['content'];
 
-      const resultado = {
-        lancamentos,
-        total: response['totalElements']
-      };
+        const resultado = {
+          lancamentos,
+          total: response['totalElements']
+        };
 
-      return resultado;
-    });
+        return resultado;
+      });
   }
 
   excluir(codigo: number): Promise<void> {
@@ -75,7 +76,51 @@ export class LancamentoService {
     headers = headers.append('Content-Type', 'application/json');
 
     return this.http.post<Lancamento>(this.lancamentosUrl, lancamento, { headers })
-    .toPromise();
+      .toPromise();
+  }
+
+  atualizar(lancamento: Lancamento): Promise<Lancamento> {
+    let headers = new HttpHeaders();
+    headers = headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    headers = headers.append('Content-Type', 'application/json');
+
+    return this.http.put<Lancamento>(this.lancamentosUrl, lancamento, { headers })
+      .toPromise()
+      .then(response => {
+        const lancamentoAlterado = response as Lancamento;
+
+        this.converterStringsParaDatas([lancamentoAlterado]);
+
+        return lancamentoAlterado;
+      });
+  }
+
+  buscarPorCodigo(codigo: number): Promise<Lancamento>  {
+    let headers = new HttpHeaders();
+    headers = headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    headers = headers.append('Content-Type', 'application/json');
+
+    return this.http.get<Lancamento>(`${this.lancamentosUrl}/${codigo}`, { headers })
+    .toPromise()
+    .then(response => {
+      const lancamentoRecuperado = response as Lancamento;
+
+      this.converterStringsParaDatas([lancamentoRecuperado]);
+
+      return lancamentoRecuperado;
+    });
+  }
+
+  private converterStringsParaDatas(lancamentos: Lancamento[]) {
+    for (const lancamento of lancamentos) {
+      lancamento.dataVencimento = moment(lancamento.dataVencimento,
+        'YYYY-MM-DD').toDate();
+
+      if (lancamento.dataPagamento) {
+        lancamento.dataPagamento = moment(lancamento.dataPagamento,
+          'YYYY-MM-DD').toDate();
+      }
+    }
   }
 
 }
