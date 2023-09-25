791c442a622e4c4f0b9102e86d0c5044df831ffc
EdisonMartins <martinsegmj@gmail.com>
2023-09-25 05:36:28 -0300

    18.13 - Refatorando as rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,24 +13,9 @@ import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { RouterModule, Routes } from '@angular/router';
-
-// Componentes
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(localePT);
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
 
 @NgModule({
   declarations: [
@@ -40,9 +25,8 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule,
-    RouterModule.forRoot(routes),
 
+    AppRoutingModule,
     GrowlModule,
     CoreModule,
     LancamentosModule,
