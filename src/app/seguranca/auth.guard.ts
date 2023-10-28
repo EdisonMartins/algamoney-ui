3e3924eacd2025e079563f8278a5e13c656323b9
EdisonMartins <martinsegmj@gmail.com>
2023-10-28 01:19:37 -0300

    19.15 - AuthGuard - Usuários deslogados

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -9,17 +9,34 @@ export class AuthGuard implements CanActivate {
   constructor(
     private auth: AuthService,
     private router: Router
-  ) {}
+  ) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> | Observable<boolean> {
+    if (this.auth.isAccessTokenInvalido()) {
+      console.log('Navegação com access token inválido. Obtendo novo token...');
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return this.auth.obterNovoAccessToken()
+        .then(() => {
+          if (this.auth.isAccessTokenInvalido()) {
+            this.router.navigate(['/login']);
+            return false;
+          }
 
-    if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
+          return this.podeAcessarRota(route.data.roles);
+        });
+    }
+
+    return this.podeAcessarRota(route.data.roles);
+  }
+
+
+
+  podeAcessarRota(roles: string[]): boolean {
+    if (roles && !this.auth.temQualquerPermissao(roles)) {
       this.router.navigate(['/nao-autorizado']);
       return false;
     }
 
     return true;
   }
+
 }
